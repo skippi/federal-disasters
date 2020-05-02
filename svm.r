ff04ca046774c559b7803e65195aa0f422c0e66a
skippi <jbtcao@gmail.com>
2020-05-02 16:36:32 -0500

    Added svm implementation
    - Implemented date preprocessing. Breaks date into month, day, and year.

--- svm.r
+++ svm.r
@@ -1,1 +1,34 @@
+library(tidyr)
+
+replace.date = function(df, pred_name) {
+  names = c(sprintf("%s.Month", pred_name), sprintf("%s.Day", pred_name), sprintf("%s.Year", pred_name))
+  
+  splited = separate(df, pred_name, names, "/", convert=TRUE)
+  
+  return(splited)
+}
+
+omit.cols = function(df, names) {
+  return(df[, !(names(df) %in% names)])
+}
+
+omitted_cols = c(
+  "Declaration.Number",
+  "Declaration.Type",
+  "Disaster.Title"
+)
+
 data = read.csv("data.csv")
+data = omit.cols(data, omitted_cols)
+data = replace.date(data, "Declaration.Date")
+data = replace.date(data, "Start.Date")
+data = replace.date(data, "End.Date")
+data = replace.date(data, "Close.Date")
+
+set.seed(0)
+train <- sample(nrow(data), 0.25 * nrow(data))
+
+library(e1071)
+svm.fit = svm(Disaster.Type ~ ., data=data[train,], kernel="radial", gamma=1, cost=1)
+svm.pred <- predict(svm.fit,newdata=data[-train,])
+mean(svm.pred != data[-train,]$Disaster.Type) # misclassification error rate
