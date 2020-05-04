c36147063582a5fddf85de820ecacb316327cdc0
skippi <jbtcao@gmail.com>
2020-05-04 00:12:17 -0500

    Added start.month, duration, and start.season
    - Testing shows these improve the results.

--- svm.r
+++ svm.r
@@ -1,15 +1,18 @@
+rm(list=ls())
+
 library(tidyr)
 
-replace.date = function(df, pred_name) {
-  names = c(sprintf("%s.Month", pred_name), sprintf("%s.Day", pred_name), sprintf("%s.Year", pred_name))
-  
-  splited = separate(df, pred_name, names, "/", convert=TRUE)
-  
-  return(splited)
-}
+Date.season = function(DATES) {
+  winter = as.Date("2012-12-15") # Winter Solstice
+  spring = as.Date("2012-3-15") # Spring Equinox
+  summer = as.Date("2012-6-15") # Summer Solstice
+  fall = as.Date("2012-9-15") # Fall Equinox
 
-omit.cols = function(df, names) {
-  return(df[, !(names(df) %in% names)])
+  d = as.Date(strftime(DATES, format="2012-%m-%d"))
+
+  ifelse(winter <= d | d < spring, "Winter",
+    ifelse(spring <= d & d < summer, "Spring",
+      ifelse(summer <= d & d < fall, "Summer", "Fall")))
 }
 
 omitted_cols = c(
@@ -19,29 +22,27 @@ omitted_cols = c(
   "Close.Date"
 )
 
+## Cleaning
+
 data = read.csv("data.csv")
-data = omit.cols(data, omitted_cols)
-data = replace.date(data, "Declaration.Date")
-data = replace.date(data, "Start.Date")
-data = replace.date(data, "End.Date")
+data = data[, !(names(data) %in% omitted_cols)]
+data = drop_na(data)
+data$Declaration.Date = as.Date(data$Declaration.Date, tryFormats="%m/%d/%Y")
+data$Start.Date = as.Date(data$Start.Date, tryFormats="%m/%d/%Y")
+data$End.Date = as.Date(data$End.Date, tryFormats="%m/%d/%Y")
+
+## EDA Predictors
+
+data$Start.Month = as.factor(months(data$Start.Date))
+data$Start.Season = as.factor(Date.season(data$Start.Date))
+data$Duration = as.numeric(data$End.Date - data$Start.Date)
+
+## Training
 
 set.seed(0)
-train <- sample(nrow(data), 0.25 * nrow(data))
+train <- sample(nrow(data), 0.2 * nrow(data))
 
 library(e1071)
-svm.fit = svm(Disaster.Type ~ ., data=data[train,], kernel="radial", gamma=1, cost=1)
+svm.fit = svm(Disaster.Type ~ ., data=data[train,], kernel="polynomial", gamma=1)
 svm.pred <- predict(svm.fit,newdata=data[-train,])
 mean(svm.pred != data[-train,]$Disaster.Type) # misclassification error rate
-
-
-## Tune
-
-svm.tuned = tune(svm, Disaster.Type ~ ., data=data[train,],
-                 kernel="radial",
-                 ranges=list(
-                   cost=c(0.1,1,10,100,1000),
-                   gamma=c(0.5,1,2,3,4))
-                 )
-
-summary(tune.out)
-
